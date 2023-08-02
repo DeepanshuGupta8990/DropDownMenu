ed3d4e491cd8471fe5fe7193752dcfa71f2deb31
Deepanshu Gupta <DEEPANSHUGUPTA899@GMAIL.COM>
2023-08-02 15:17:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const card = document.querySelector('.card');
+const cardToggle = document.querySelector(".toggle");
+cardToggle.addEventListener("click",()=>{
+    card.classList.toggle("active");
+})
